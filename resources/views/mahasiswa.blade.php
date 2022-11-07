98d93a1edc4cf04cde61067c683f9044f888fcec
nafimulyoo <rxnafimulyo@gmail.com>
2022-11-07 20:19:01 +0700

    create table:mahasiswa

---
+++ resources/views/mahasiswa.blade.php
@@ -0,0 +1,17 @@
+@extends('app')
+@section('content')
+<table>
+    <tr>
+        <th>No</th>
+        <th>NIM</th>
+        <th>Nama</th>
+    </tr>
+    @foreach ($mahasiswa as $mhs)
+    <tr>
+        <td>{{ $mhs->id }}</td>
+        <td>{{ $mhs->NIM }}</td>
+        <td>{{ $mhs->nama }}</td>
+    </tr>
+    @endforeach
+</table>
+@endsection
